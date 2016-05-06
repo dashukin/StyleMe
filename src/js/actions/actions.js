31509c157ab1287da8e5d8b18a24ee428e10bb4d
dashukin <dashukin@gmail.com>
2016-05-06 10:18:43 +0300

    Update actions

--- src/js/actions/actions.js
+++ src/js/actions/actions.js
@@ -3,7 +3,7 @@ import AppDispatcher from '../dispatchers/dispatcher';
 import AppConstants from '../constants/constants';
 
 export default {
-	switchView: (viewType) => {
+	switchView: viewType => {
 		AppDispatcher.dispatch({
 			actionType: AppConstants.SWITCH_VIEW,
 			payload: {viewType}
@@ -14,7 +14,7 @@ export default {
 			actionType: AppConstants.ADD_FIELD
 		});
 	},
-	removeField: (key) => {
+	removeField: key => {
 		AppDispatcher.dispatch({
 			actionType: AppConstants.REMOVE_FIELD,
 			payload: {key}
@@ -24,6 +24,24 @@ export default {
 		AppDispatcher.dispatch({
 			actionType: AppConstants.ADD_INPUT_VALUE,
 			payload: {key, value}
+		});
+	},
+	setEnable: enable => {
+		AppDispatcher.dispatch({
+			actionType: AppConstants.SET_ENABLE,
+			payload: {enable}
+		});
+	},
+	setAutoUpdate: autoUpdate => {
+		AppDispatcher.dispatch({
+			actionType: AppConstants.SET_AUTO_UPDATE,
+			payload: {autoUpdate}
+		});
+	},
+	setUpdateFrequency: updateFrequency => {
+		AppDispatcher.dispatch({
+			actionType: AppConstants.SET_UPDATE_FREQUENCY,
+			payload: {updateFrequency}
 		});
 	},
 	saveConfiguration: () => {
