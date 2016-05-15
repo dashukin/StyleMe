70527d28a0807bac2f9aac3c385d6a338e0aa0a3
dashukin <dashukin@gmail.com>
2016-05-15 04:05:42 +0300

    Add autocomplete component to handle ignored stylesheets

--- src/js/components/app-stylesheets-item-component.js
+++ src/js/components/app-stylesheets-item-component.js
@@ -4,9 +4,9 @@
 import React from 'react';
 import AppActions from '../actions/actions';
 import TextField from 'material-ui/TextField';
+import AutoComplete from 'material-ui/AutoComplete';
 import IconButton from 'material-ui/IconButton';
 import DeleteIcon from 'material-ui/svg-icons/navigation/close';
-
 
 class StylesheetItemComponent extends React.Component {
 
@@ -22,7 +22,44 @@ class StylesheetItemComponent extends React.Component {
 
 	render () {
 
-		let {stylesheetConfig} = this.props;
+		let {stylesheetConfig, originalStyleSheets} = this.props;
+
+		let fieldStyles = {
+			width: '100%'
+		};
+
+		let autoCompleteListStyles = {
+			background: '#fff'
+		};
+
+		let enableAutoComplete = (Object.prototype.toString.call(originalStyleSheets).slice(8, -1) === 'Array') && originalStyleSheets.length;
+
+		let autoCompleteSourceData = enableAutoComplete
+			? 	originalStyleSheets.reduce((data, {src, key}) => {
+					data.push({
+						text: src,
+						value: src,
+						styleSheetKey: key
+					});
+					return data;
+				}, [])
+			: [];
+
+		let autoCompleteComponent = enableAutoComplete
+			? <AutoComplete
+				hintText="Disable any existing stylesheet?"
+				searchText={stylesheetConfig.ignoredStyleSheet}
+				filter={AutoComplete.caseInsensitiveFilter}
+				fullWidth={true}
+				openOnFocus={true}
+				dataSource={autoCompleteSourceData}
+				onUpdateInput={this.onUpdateInput}
+				onNewRequest={this.onNewRequest}
+				maxSearchResults={5}
+				style={fieldStyles}
+				listStyle={autoCompleteListStyles}
+				menuStyle={{background: '#fff'}} />
+			: '';
 
 		return (
 			<div className="stylesheet-item">
@@ -31,9 +68,11 @@ class StylesheetItemComponent extends React.Component {
 					ref="stylesheet-input"
 					hintText="path/to/css/file"
 					defaultValue={stylesheetConfig.src}
-					style={{width: '80%'}}
-					onChange={this.changeHandler}
+					style={fieldStyles}
+					onChange={this.handleStyleInputChange}
 				/>
+
+				{autoCompleteComponent}
 
 				<IconButton
 					style={{width: '20%'}}
@@ -53,7 +92,7 @@ class StylesheetItemComponent extends React.Component {
 		AppActions.removeField(fieldKey);
 	}
 
-	changeHandler = (e) => {
+	handleStyleInputChange = (e) => {
 
 		let fieldKey = this.props.stylesheetConfig.key;
 
@@ -62,6 +101,19 @@ class StylesheetItemComponent extends React.Component {
 			value: e.target.value
 		});
 		
+	}
+
+	onUpdateInput = (enteredValue, dataSource) => {
+
+	}
+
+	onNewRequest = (selectedData, index) => {
+
+		AppActions.addIgnoredStyleSheetKey({
+			key: this.props.stylesheetConfig.key,
+			ignoredKey: selectedData.styleSheetKey,
+			value: selectedData.text
+		});
 	}
 
 }
